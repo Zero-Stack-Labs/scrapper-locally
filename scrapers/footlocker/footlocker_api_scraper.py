ceac0299c8d5580e62c4cc27b7dbbc813c5105ea
carlossanchez <carlos.sanchez@seeri.co>
2025-07-28 22:10:40 -0500

    change fallback token

--- scrapers/footlocker/footlocker_api_scraper.py
+++ scrapers/footlocker/footlocker_api_scraper.py
@@ -19,7 +19,7 @@ class FootlockerApiScraper:
         self.headers = {
             'accept': 'application/json',
             'accept-language': 'en-US,en;q=0.9',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
             'x-kpsdk-ct': x_kpsdk_ct
         }
 
